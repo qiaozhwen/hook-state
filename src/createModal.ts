dd2ff4caac300da505bb6e298b8ba4f494148cee
qiaozhen03 <qiaozhen03@58.com>
2021-09-28 10:40:43 +0800

    add type

--- src/createModal.ts
+++ src/createModal.ts
@@ -1,25 +1,31 @@
 import React,{useState, useEffect} from 'react'
 export const createContainer = (initState)=>{
     let globalState = initState
-    const listeners = Object.fromEntries(Object.keys(initState).map(key=>[key, new Set()]))
+    const listeners:any = Object.keys(initState).map(key=>{
+        let a = {}
+        a[key] = new Set()
+        return a
+    })
     const setGlobalState = (key, nextValue) => {
         if(typeof nextValue==='function'){
             globalState = { ...globalState, [key]: nextValue(globalState[key]) };
         }else{
             globalState = { ...globalState, [key]: nextValue };
         }
-        listeners[key].forEach((listener:any)=> listener())
+        for(let listener of listeners[key]){
+            listener()
+        }
     }
     const useSetGlobalState = (key)=> {
         const [state, setState] = useState<any>(globalState[key])
-        // useEffect(() => {
-        //     const listener = () => {
-        //         setState(globalState[key]);
-        //     };
-        //     listeners[key].add(listener);
-        //     listener(); // in case it's already changed
-        //     return () => listeners[key].delete(listener); // cleanup
-        // }, []);
+        useEffect(() => {
+            const listener = () => {
+                setState(globalState[key]);
+            };
+            listeners[key].add(listener);
+            listener(); // in case it's already changed
+            return () => listeners[key].delete(listener); // cleanup
+        }, []);
         return [state, (newValue)=>setGlobalState(key, newValue)]
     }
     return {
