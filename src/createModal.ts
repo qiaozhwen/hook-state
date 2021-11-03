7c613a175269c853912c9a4f1a6d328cf135a402
qiaozhen03 <qiaozhen03@58.com>
2021-11-03 18:44:06 +0800

    add class

--- src/createModal.ts
+++ src/createModal.ts
@@ -10,23 +10,18 @@ type listenerType = {
 interface setGlobalState<T> {
   (key: string): [initStateType, any];
 }
-
 export function createContainer<T>(initState: initStateType) {
   let globalState = initState
-  const listeners: listenerType = Object.keys(initState).map(key => {
-    let a = {}
-    a[key] = new Set()
-    return a
-  })
-  const setGlobalState = <T>(key, nextValue) => {
+  const listeners: listenerType = Object.fromEntries(Object.entries(initState).map(([key, value])=>[key, new Set([])]))
+  const setGlobalState = <T>(key:any, nextValue:any) => {
     if (typeof nextValue === 'function') {
       globalState = {...globalState, [key]: nextValue(globalState[key])};
     } else {
       globalState = {...globalState, [key]: nextValue};
     }
-    for (let listener of listeners[key]) {
+    listeners[key].forEach((listener:any)=>{
       listener()
-    }
+    })
   }
   const useSetGlobalState: setGlobalState<T> = (key) => {
     const [state, setState] = useState<any>(globalState[key])
@@ -38,24 +33,27 @@ export function createContainer<T>(initState: initStateType) {
       listener(); // in case it's already changed
       return () => listeners[key].delete(listener); // cleanup
     }, []);
-    return [state, (newValue) => setGlobalState(key, newValue)]
+    return [state, (newValue:any) => {
+      setGlobalState(key, newValue);
+    }]
   }
   const releaseState = (WrapComponent: any) => {
-    class ReturnComponent extends WrapComponent {
-      constructor(props) {
+    class ReturnComponent extends React.Component<any, any> {
+      constructor(props:any) {
         super(props);
       }
-      changeLocalState (data) {
+      changeLocalState (data:any) {
         // change global state
         const {key, value} = data
         globalState[key] = value;
         for (let listener of listeners[key]) {
           listener()
         }
-        WrapComponent.render();
+        this.render();
       }
       render() {
         const newProps = {...this.props, ...globalState, changeLocalState:this.changeLocalState}
+        console.log('22222', React.cloneElement(WrapComponent, {...newProps}))
         // combine with hook state
         return React.cloneElement(WrapComponent, {...newProps})
       }
